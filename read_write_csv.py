9425860c7fdea52c5e7f5771ad34c00b715cdd64
Leonju <juya@shangaitech.edu.cn>
2023-03-11 01:47:28 +0800

    delete used files

--- read_write_csv.py
+++ read_write_csv.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import csv
 #读取csv文件内容
-def ReadCsv():
-    con = pd.read_csv(input('path:'),)
-    print(con)
+def ReadCsv(path):
+    con = pd.read_csv(path)
+    return con
 #追加写入csv
 def addCsv():
     with open(input('path:'), 'a') as f:
         writer = csv.writer(f)
         writer.writerow([input('Cat_Name:'),input('Location:'),input('Date:'),input("Time:")])
 
-ReadCsv()
+
 
 
 
