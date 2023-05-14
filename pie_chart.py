2add80c427ff5f4ff8f0234e69d1435b4d76ff81
Conamara <126929983+Conamara@users.noreply.github.com>
2023-05-14 18:25:26 +0800

    add a function to plot a specific area

--- pie_chart.py
+++ pie_chart.py
@@ -13,7 +13,8 @@ plt.rcParams['font.sans-serif']=['Microsoft JhengHei']
 功能: 从数据库中检索指定日期区间内的记录,并统计每只猫的活跃度(出现次数占比),绘制饼图
 输入参数: 日期下限,日期上限
 '''
-def pie_chart(date1,date2):
+#校园范围
+def school_pie_chart(date1,date2):
     # 读取原始数据
     df = rw.ReadCsv()
     # 截取date1和date2间的记录
@@ -32,18 +33,18 @@ def pie_chart(date1,date2):
             startangle = 90,
             colors = colors)
     title = f"{date1}至{date2}的活跃度统计"
-    plt.set_title(title)
+    plt.title(title)
     plt.axis('equal')
     plt.legend(loc='upper right')
     plt.show()
     return
 
-
-def pie_chart_for_ui(date, fig, ax):
+def area_pie_chart(date1,date2,area):
     # 读取原始数据
     df = rw.ReadCsv()
     # 截取date1和date2间的记录
-    df = s.search_Date(df,date,date)
+    df = s.search_Date(df,date1,date2)
+    df=s.search_Place(df,area)
     # 对每只猫做名称检索，统计其出现次数
     list = []
     for a in range(1,11):
@@ -51,13 +52,18 @@ def pie_chart_for_ui(date, fig, ax):
       df1 = s.search_Name(df,name)
       list.append(df1.shape[0])
       colors = ["#55efc4", "#81ecec", "#74b9ff", "#a29bfe", "#dfe6e9", "#ffeaa7", "#fab1a0", "#fd79a8", "#e17055", "#fdcb6e"]
-    ax.pie(x = list, 
-            labels = ['cat1','cat2','cat3','cat4','cat5','cat6','cat7','cat8','cat9','cat10'], 
-            shadow = False, 
-            autopct = '%1.1f%%', 
+    plt.pie(x = list,
+            labels = ['cat1','cat2','cat3','cat4','cat5','cat6','cat7','cat8','cat9','cat10'],
+            shadow = False,
+            autopct = '%1.1f%%',
             startangle = 90,
             colors = colors)
-    ax.set_title(f"当日的活跃度统计")
-    ax.axis('equal')
-    ax.legend(loc='upper right')
+    title = f"{date1}至{date2}的活跃度统计"
+    plt.title(title)
+    plt.axis('equal')
+    plt.legend(loc='upper right')
+    plt.show()
     return
+
+if __name__=='__main__':
+	area_pie_chart('2023.1.1','2023.1.3','area1')
