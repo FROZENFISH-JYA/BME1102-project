ff97da063147321d993df6c544a94a4809c0187e
Conamara <126929983+Conamara@users.noreply.github.com>
2023-05-19 10:54:36 +0800

    进一步完善注释

--- alarm.py
+++ alarm.py
@@ -46,24 +46,24 @@ def cat_miss(name):
     for i in three_dates:#对每天的出现次数查询
         n=se.search_1_date(i)
         three_days_list.append(n)
-    if sum(three_days_list)==0:
-        return 'missing warning'
+    if sum(three_days_list)==0:#连续三天不出现
+        return 'missing_warning'
     else:
-        return 'normal situation'
+        return 'normal_situation'
 
 #对一只猫的死亡预警：（5天不出现）
 def cat_death(name):
     date_now = current_date()  # 获取当天日期
     five_dates=five_days_before(date_now)
     k= date_now.split('.')
-    three_days_list=[]
+    five_days_list=[]
     for i in five_dates:#对每天的出现次数查询
         n=se.search_1_date(i)
-        three_days_list.append(n)
-    if sum(three_days_list)==0:
-        return 'death warning'
+        five_days_list.append(n)
+    if sum(five_days_list)==0:#连续5天不出现
+        return 'death_warning'
     else:
-        return 'normal situation'
+        return 'normal_situation'
 
 #生病预警(出现次数衰减30%以上）
 def cat_ill(name):
@@ -76,12 +76,12 @@ def cat_ill(name):
         three_days_list.append(n)
     mean_recent=sum(three_days_list)/3#获取3天内猫活动次数的平均值
     date_30days_ago=date_before_30days(date_now)
-    last_month=dg.data_generate2(date_30days_ago[:7],name)
-    mean_normal=sum(last_month)/len(last_month)
+    last_month=dg.data_generate2(date_30days_ago[:7],name)#获取上个月数据
+    mean_normal=sum(last_month)/len(last_month)#计算上个月活动次数平均值
     if (mean_normal-mean_recent)/mean_normal>0.3:#如果活动次数减少30%以上认定为生病预警
-        return 'ill warning'
+        return 'ill_warning'
     else:
-        return 'normal situation'
+        return 'normal_situation'
 
 #对小区域而言的危险预警
 def area_danger(area):
@@ -97,14 +97,15 @@ def area_danger(area):
     last_month = dg.data_generate5(date_30days_ago[:7], area)#获取上个月该区域猫活动次数的平均值作为正常指标
     mean_normal=sum(last_month)/len(last_month)
     if (mean_normal-mean_recent)/mean_normal>0.3:#如果该地点猫活动次数减少30%以上认定为区域危险
-       return 'area warning'
+       return 'area_warning'
     else:
-        return 'normal situation'
+        return 'normal_situation'
 
-#定义一个函数对所有数据检测：
+#定义一个函数对所有数据检测，并将报告写入txt文件
 def test_all():
     Note = open('alarm_report.txt', mode='a')
-    Note.write('name  miss  ill  death')
+    Note.write('name   miss    ill    death')
+    #将所有的猫代入上面所有函数检测
     for i in ['cat1','cat2','cat3','cat4','cat5','cat6','cat7','cat8','cat9','cat10']:
         Note.write(f'\n {i}  {cat_miss(i)}  {cat_ill(i)}  {cat_death(i)}')
     Note.close()
