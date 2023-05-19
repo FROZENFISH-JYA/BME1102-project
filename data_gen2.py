abb46bcd1d5b8ab6a2e70ac0343485b66a03b8f2
Conamara <126929983+Conamara@users.noreply.github.com>
2023-05-19 10:55:36 +0800

    进一步完善注释

--- data_gen2.py
+++ data_gen2.py
@@ -5,8 +5,9 @@ from matplotlib import pyplot as plt
 import read_write_csv as r
 import data_generate as dg
 
+'''先定义两个对数据预处理的函数'''
 #通过调用data_gen1,计算一段日期内每天活动次数的分布
-def data_gen2(date1, date2, name):
+def data_gen2(date1, date2, name):#输入起始日期date1,结束日期date2
 		date1=dt.datetime.strptime(date1,"%Y.%m.%d").date()
 		date2=dt.datetime.strptime(date2,"%Y.%m.%d").date()
 		#日期上限
@@ -30,10 +31,12 @@ def date_before(date,days):#date为起始日期，days为多少天前
 	year = int(date_list[0])
 	month = int(date_list[1])
 	day = int(date_list[2])
-	date_days_ago = dt.datetime(year, month, day) - dt.timedelta(days=days)
+	date_days_ago = dt.datetime(year, month, day) - dt.timedelta(days=days)#日期减法
 	date_days_ago = date_days_ago.strftime('%Y.%m.%d')
 	return date_days_ago
-def plot_distribution(date,name):#Y0为平均值，Y为所查值
+
+'''以下为画图的函数'''
+def plot_day_distribution(date,name):#Y0为平均值，Y为所查值
 		X = list(range(1, 25))#一天24小时
 		#获取前30天的起始和终止日期
 		start_date=date_before(date,30)
@@ -49,6 +52,25 @@ def plot_distribution(date,name):#Y0为平均值，Y为所查值
 		plt.ylabel('num')
 		plt.title(f'mean distribution during " {start_date} and  {end_date}')
 		plt.show()
-if __name__=='__main__':
 
-	plot_distribution('2023.2.2','cat1')
+#某个月每天折线图
+def plot_month_distribution(month,name):#输入月份（如“2023.1“），和猫的名字
+	Y = dg.data_generate2(month, name)  # 获取所查日期数据
+	X = list(range(1, len(Y)+1))
+	plt.plot(X, Y)  # 画
+	plt.xlabel('day')
+	plt.ylabel('num')
+	plt.title(f'month distribution of {name} ')
+	plt.show()
+#某年每月折线图
+def plot_year_distribution(year,name):#输入年份（如“2023”），和猫的名字
+	Y = dg.data_generate3(year, name)  # 获取所查日期数据
+	X = list(range(1, len(Y)+1))
+	plt.plot(X, Y)  # 画
+	plt.xlabel('month')
+	plt.ylabel('num')
+	plt.title(f'year distribution of {name} ')
+	plt.show()
+
+if __name__=='__main__':
+	plot_year_distribution('2023', 'cat1')
